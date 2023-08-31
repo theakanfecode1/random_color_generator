f7db564ecd26b71d119015f2b7992ac29c2e8c5b
theakanfecode1 <danielogundiranakanfe@gmail.com>
2023-08-31 19:14:52 +0100

    feat: App Styling

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:random_color_generator/screens/color_change_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Color Generator',
+      theme: ThemeData(
+        fontFamily: ('AlbertSans'),
+        primarySwatch: Colors.blue,
+      ),
+      home: const ColorChangeScreen(),
+    );
+  }
+}
---
+++ lib/screens/color_change_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorChangeScreen extends StatelessWidget {
+  const ColorChangeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
